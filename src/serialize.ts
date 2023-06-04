92bd3b27a907e2d16eca88273e0e60b314460d41
Vladyslav Usenko <vu4848@gmail.com>
2023-06-04 18:56:09 +0300

    add custom size of impl

--- src/serialize.ts
+++ src/serialize.ts
@@ -35,14 +35,11 @@ const handler = {
                 const temp = new Uint8Array(target.buff, offset, property.byteSize);
                 return new TextDecoder().decode(temp);
             }
-            case 'object': {
-                return new Proxy(target.properties[propertyName].nestedBbo!, handler);
-            }
             case 'null': {
                 return null;
             }
             default: {
-                throw new Error('not implemented');
+                return new Proxy(target.properties[propertyName].nestedBbo!, handler);
             }
         }
     }
@@ -91,7 +88,17 @@ export function process(object: Record<string, unknown>, buff: ArrayBuffer, view
             }
             case 'object': {
                 if (Array.isArray(value)) {
-                    throw new Error('not implemented');
+                    const nestedBbo: BufferBackedObject = {
+                        buff,
+                        properties: {},
+                    };
+                    bbo.properties[key] = {
+                        valueType: 'array',
+                        offset,
+                        nestedBbo,
+                    };
+                    offset += process(value as Record<number, unknown>, buff, view, nestedBbo, offset);
+                    break;
                 }
 
                 if (value === null) {
@@ -124,8 +131,31 @@ export function process(object: Record<string, unknown>, buff: ArrayBuffer, view
     return offset;
 }
 
+function sizeOf(object: Record<string, unknown>): number {
+        const objectList = [];
+        const stack: Array<unknown> = [object];
+        let bytes = 0;
+    
+        while (stack.length) {
+            var value = stack.pop();
+    
+            if(typeof value === 'boolean'){
+                bytes += 4;
+            }else if(typeof value === 'string'){
+                
+                bytes += (value as string).length * 4;
+            }else if(typeof value === 'number'){
+                bytes += 8;
+            }else if(typeof value === 'object' && objectList.indexOf( value ) === -1){
+                objectList.push(value);
+                for(var key in value) stack.push((value as Record<string, unknown>)[key]);
+            }
+        }
+        return bytes;
+}
+
 export function serialize(object: Record<string, unknown>): Record<string, unknown> {
-    const size = sizeof(object);
+    const size = sizeOf(object);
     const buff = new ArrayBuffer(size);
 
     const bbo: BufferBackedObject = {
@@ -137,7 +167,6 @@ export function serialize(object: Record<string, unknown>): Record<string, unkno
 
     return new Proxy(bbo, handler);
 }
-
 
 function test() {
     const obj = {
@@ -153,7 +182,8 @@ function test() {
             anotherKey: {
                 with: "nested value"
             }
-        }
+        },
+        array: [1, 2, { three: "value from an object inside an array with a fucking emoji 🦴" }],
     };
     const bufferedObj = serialize(obj);
     console.log(bufferedObj);
@@ -164,6 +194,9 @@ function test() {
     console.log(bufferedObj.someString);
     console.log(bufferedObj.someUnicodeString);
     console.log(bufferedObj.pureUnicodeEmojisString);
+    console.log((bufferedObj.array as Array<number|Record<string, unknown>>)[0]);
+    console.log((bufferedObj.array as Array<number|Record<string, unknown>>)[1]);
+    console.log(((bufferedObj.array as Array<number|Record<string, unknown>>)[2] as Record<string, unknown>).three);
     console.log((bufferedObj.nestedObject as Record<string, unknown>).key);
     console.log(((bufferedObj.nestedObject as Record<string, unknown>).anotherKey as Record<string, unknown>).with);
 }
